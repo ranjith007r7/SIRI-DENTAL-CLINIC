400720b82ec2602bd18d21a23a4b105de1935fa4
Ranjith <126404922+ranjith007r7@users.noreply.github.com>
2026-01-02 13:44:23 +0530

    Add files via upload
    Initial working version of SIRI Dental Clinic system

---
+++ public/siri-dental-backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+//require("./utils/reminderJob");
+
+
+const appointmentRoutes = require("./routes/appointments");
+
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/appointments", appointmentRoutes);
+
+const adminRoutes = require("./routes/admin");
+
+app.use("/api/admin", adminRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("SIRI Dental Backend is running");
+});
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("✅ MongoDB connected");
+    })
+    .catch((err) => {
+        console.error("❌ MongoDB connection error:", err);
+    });
+    
+const availabilityRoutes = require("./routes/availability");
+app.use("/api/availability", availabilityRoutes);
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
